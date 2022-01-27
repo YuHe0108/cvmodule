617522579dee47f57e0732dea78be23bf555c1f5
YuHe <1941254847@qq.com>
2022-01-27 09:37:54 +0800

    Add files via upload
    essential for making python packages

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cv_module",
+    version="0.0.4",
+    author="YuHe",
+    author_email="1941254847@qq.com",
+    description="modules related to deep learning in computer vision",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YuHe0108/cv_module",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
